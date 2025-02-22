80e0b8e47f932d217bc38bc5b6962ade081e131a
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2025-02-22 15:00:39 -0500

    Initial Commit

---
+++ ContactExplorer/ContactExplorerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ContactExplorerApp.swift
+//  ContactExplorer
+//
+//  Created by callum on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct ContactExplorerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ ContactExplorerTests/ContactExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactExplorerTests.swift
+//  ContactExplorerTests
+//
+//  Created by callum on 2025-02-22.
+//
+
+import Testing
+@testable import ContactExplorer
+
+struct ContactExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
