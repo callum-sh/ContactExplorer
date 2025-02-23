808b58129522f6483a69d0cb470d6ce962340c37
harvinparknight <50000691+harvinparknight@users.noreply.github.com>
2025-02-23 00:51:39 -0500

    culled some fat

---
+++ ContactExplorer/Screens/Home.swift
@@ -0,0 +1,17 @@
+//
+//  Home.swift
+//  ContactExplorer
+//
+//  Created by Harvin Park on 2/23/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome Home!")
+        }
+    }
+}
